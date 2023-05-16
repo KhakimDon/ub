47544db835b37112ca90149f0631829ca0626d33
khakimdon <almamedovkh@mail.ru>
2023-05-16 15:08:48 +0500

    third

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navigation btm-nav bg-[#333]">
 
-    <button :class="{active: this.$route.path == '/'}" data-page="/"  @click="this.$router.push(`/`), color()" class="text-[white] ">
+    <button  data-page="/"  @click="this.$router.push(`/`), color()" class="text-[white] ">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -18,7 +18,7 @@
       </svg>
     </button>
 
-    <button :class="{active: this.$route.path == '/about'}" data-page="/about"  @click="this.$router.push(`/about`),color()" class=" text-[white]">
+    <button data-page="/about"  @click="this.$router.push(`/about`),color()" class=" text-[white]">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
@@ -26,7 +26,7 @@
 
     </button>
 
-    <button :class="{active: this.$route.path == '/auth'}" data-page="/auth" @click="this.$router.push(`/auth`),color()" class=" text-[white]">
+    <button  data-page="/auth" @click="this.$router.push(`/auth`),color()" class=" text-[white]">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 </svg>
@@ -46,6 +46,7 @@ export default {
     },
     mounted(){
         console.log('asda');
+        console.log(window.location.pathname);
        
     },
     methods:{
